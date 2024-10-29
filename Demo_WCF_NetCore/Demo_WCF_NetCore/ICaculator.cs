ac53924f0b32e751043c6fd0db124617c99e6338
Hvuthai <hoangvuthai.bl1@gmail.com>
2024-10-30 01:52:40 +0700

    push

---
+++ Demo_WCF_NetCore/Demo_WCF_NetCore/ICaculator.cs
@@ -0,0 +1,15 @@
+﻿namespace Demo_WCF_NetCore
+{
+    [ServiceContract]
+    public interface ICaculator
+    {
+        [OperationContract]
+        double Add(double n1, double n2);
+        [OperationContract]
+        double Subtract(double n1, double n2);
+        [OperationContract]
+        double Multiply(double n1, double n2);
+        [OperationContract]
+        double Divide(double n1, double n2);
+    }
+}
---
+++ Demo_WCF_NetCore_Client/Demo_WCF_NetCore_Client/Program.cs
@@ -0,0 +1,41 @@
+﻿using ServiceReference1;
+
+namespace Demo_WCF_NetCore_Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CaculatorClient client = new CaculatorClient();
+            // Step 2: Call the service operations.
+            // Call the Add service operation.
+            double value1 = 100.00D;
+            double value2 = 15.99D;
+            double result = client.AddAsync(value1, value2).Result;
+            Console.WriteLine("Add({0},{1}) = {2}", value1, value2, result);
+
+            // Call the Subtract service operation.
+            value1 = 145.00D;
+            value2 = 76.54D;
+            result = client.SubtractAsync(value1, value2).Result;
+            Console.WriteLine("Subtract({0},{1}) = {2}", value1, value2, result);
+
+            // Call the Multiply service operation.
+            value1 = 9.00D;
+            value2 = 81.25D;
+            result = client.MultiplyAsync(value1, value2).Result;
+            Console.WriteLine("Multiply({0},{1}) = {2}", value1, value2, result);
+
+            // Call the Divide service operation.
+            value1 = 22.00D;
+            value2 = 7.00D;
+            result = client.DivideAsync(value1, value2).Result;
+            Console.WriteLine("Divide({0},{1}) = {2}", value1, value2, result);
+
+            // Step 3: Close the client to gracefully close the connection and clean up resources.
+            Console.WriteLine("\nPress <Enter> to terminate the client.");
+            Console.ReadLine();
+            client.Close();
+        }
+    }
+}
